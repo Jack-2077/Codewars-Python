a7e240962bfcaec9cbf02ab8d2c4a400200fce64
Jack <78120697+Jack-2077@users.noreply.github.com>
2023-07-03 19:53:49 -0700

    Create number_to_string.py

---
+++ 8-kyu/number_to_string.py
@@ -0,0 +1,4 @@
+def number_to_string(num):
+#  return str(num)                  # int to string
+#  return "%s" % num                # %-formatting
+#  return "{n}".format(n=num)       # str.format() 
