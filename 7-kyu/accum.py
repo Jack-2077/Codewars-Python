036b9e02e00d11f3ba786517cd34d9ba82d79bbd
Jack <78120697+Jack-2077@users.noreply.github.com>
2023-07-17 21:19:16 -0700

    Update accum.py

--- 7-kyu/accum.py
+++ 7-kyu/accum.py
@@ -3,3 +3,7 @@ def accum(s):
     for x in range(len(s)):
         output += s[x].upper() + s[x].lower() * x + '-'
     return output[:-1]
+
+# output += (s[x] * (x+1)) + "-"
+# return output.title()[:-1]
+
