53067196acff9e8d1138019100bf86430e164834
Jack <78120697+Jack-2077@users.noreply.github.com>
2023-07-20 22:44:28 -0700

    Update square_digits.py

--- 7-kyu/square_digits.py
+++ 7-kyu/square_digits.py
@@ -2,3 +2,8 @@ def square_digits(num):
     str_num = str(num)
     arr_num = [str(int(x) ** 2) for x in str_num]
     return int(''.join(arr_num))
+
+#     ret = ""
+#     for x in str(num):
+#         ret += str(int(x)**2)
+#     return int(ret)
